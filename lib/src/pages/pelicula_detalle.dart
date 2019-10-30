da40b7e46fc316c22528179d1b97547437d19ce8
David Lodi <lodi.dev@outlook.com>
2019-10-29 23:23:19 -0600

    descipcion de la pelicula

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -11,7 +11,22 @@ class PeliculaDetalle extends StatelessWidget {
       body: CustomScrollView(
         slivers: <Widget>[
           _crearAppbar(pelicula),
-          
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10.0,
+              ),
+              _posterTitulo(context, pelicula),
+              _descripcion(pelicula),
+             _descripcion(pelicula),
+              _descripcion(pelicula),
+               _descripcion(pelicula),
+                _descripcion(pelicula),
+                 _descripcion(pelicula),
+                  _descripcion(pelicula),
+            ]),
+
+          ),
         ],
       ),
     );
@@ -20,7 +35,7 @@ class PeliculaDetalle extends StatelessWidget {
   Widget _crearAppbar(Pelicula pelicula) {
     return SliverAppBar(
       elevation: 2.0,
-      backgroundColor: Colors.deepPurple,
+      backgroundColor: Colors.transparent,
       expandedHeight: 200.0,
       floating: false,
       pinned: true,
@@ -39,4 +54,59 @@ class PeliculaDetalle extends StatelessWidget {
       ),
     );
   }
+
+  Widget _posterTitulo(BuildContext context, Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12.0),
+            child: Image(
+              image: NetworkImage(pelicula.getPosterImg()),
+              height: 150.0,
+            ),
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  pelicula.title,
+                  style: Theme.of(context).textTheme.title,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  pelicula.originalTitle,
+                  style: Theme.of(context).textTheme.subhead,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star_border, color: Colors.yellow,),
+                    Text(pelicula.voteAverage.toString(), style: Theme.of(context).textTheme.subhead,),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _descripcion(Pelicula pelicula){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Text(
+        pelicula.overview,
+        textAlign: TextAlign.justify,
+        ),
+    );
+  }
+
+
 }
